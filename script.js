d5482809cd260e493b58762f30859128b9b3c8f5
lbair39 <lilybair24@g.crookcountyschools.org>
2021-10-27 09:50:03 -0700

    Created a html and js then I worked on the js

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log('I am alive')
+
+if(2 > 40){
+    console.log('that is correct, 2 is greater than 40')
+}
+else{
+    console.log('that is correct, 2 is not greater than 40')
+};
+
+if(5 >= 24){
+    console.log('that is correct, 5 is either greater than 24 or its equal to 24')
+}
+else if(5 < 24 && 5 > 4){
+    console.log('that is correct, 5 is smaller than 24 and its greater than 4')
+}
+else{
+    console.log('that is correct, 5 is not smaller than 24 and its smaller than 4')
+};
